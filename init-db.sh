27fb38441e0c21a98d16193b3f3c34b2a4ed8d56
Alejandro <98507176+alsolucionesweb@users.noreply.github.com>
2025-11-14 00:05:04 -0500

    Fix: Some mistakes

--- init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Esperar a que MongoDB esté listo
-sleep 5
-
-# Ejecutar el script de inicialización con mongosh
-mongosh "mongodb://localhost:27017" /docker-entrypoint-initdb.d/init-db.js
